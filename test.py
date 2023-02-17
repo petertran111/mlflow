e3704a20ee1970cc45a342030f71033289d86c7a
MAMASUN <mamasun@HN-MacOS-PeterTran.local>
2023-02-17 23:46:57 +0700

    v1

--- test.py
+++ test.py
@@ -2,11 +2,10 @@
 import numpy as np
 
 import mlflow
-logged_model = 'runs:/53789294254e458a8bda39d463fa49bc/model'
+logged_model = 'runs:/db05b39e8c764acc8a78c54b29541534/model'
 
 # Load model as a PyFuncModel.
 loaded_model = mlflow.pyfunc.load_model(logged_model)
-
 
 input = np.array([6.3,0.48,0.04,1.1,0.046,30,99,0.9928,3.24,0.36,9.6])
 input = input.reshape(1, -1)
